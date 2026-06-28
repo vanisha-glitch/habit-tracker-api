d1b4eeeee4af6ace34db732edafd5614bbf84325
vanisha-glitch <vaanniii2007@gmail.com>
2026-06-29 01:34:06 +0530

    ready for deployment

--- app/main.py
+++ app/main.py
@@ -8,8 +8,6 @@ from app.routers.habits import router as habits_router   # NEW
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
-
 app.include_router(users_router)
 app.include_router(habits_router)   # NEW
 
@@ -17,4 +15,8 @@ app.include_router(habits_router)   # NEW
 @app.get("/")
 def root():
     return {"message": "Habit Tracker API is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
 
