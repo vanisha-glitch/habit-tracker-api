b23e15f2397d074f8f8fde269def40e788d4e611
vanisha-glitch <vaanniii2007@gmail.com>
2026-06-29 23:47:14 +0530

    finall app completed

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
 def get_engine():
-    database_url = os.environ.get("DATABASE_URL")
+    database_url = os.getenv("DATABASE_URL")
     if not database_url:
         raise Exception("DATABASE_URL not found in environment")
     return create_engine(database_url, pool_pre_ping=True)
